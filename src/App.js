53df4fc93d761f4431939833c1f2d20f99234112
AshokKumar1718 <ashokkumar.it17@bitsathy.ac.in>
2021-12-20 08:34:40 +0000

    react1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+class App extends Component{
+  
+  render() {
+    var value = 0;
+    var array = [1, 2, 3];
+    var even = [];
+    var odd = [];
+    var evens = false;
+    var odds = false;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function getData(val) {
+        value = val.target.value;
+    }
+
+    function handle() {
+        for (let i = 0; i < value; i++) {
+            array[i] = Math.floor(Math.random() * 100) + 1;
+        }
+        console.log(array);
+    }
+
+    function oddDisplay() {
+        if (evens === false) {
+        odds = true;
+        }
+        else {
+            evens = false;
+            odds = true;
+        }
+        let i = 0;
+        odd = [];
+        console.log(array.length);
+        while (i <= array.length) {
+            if (array[i] % 2 !== 0) {
+                if (array[i] !== undefined) {
+                    odd.push(array[i]);
+                }
+            }
+            i++;
+        }
+        console.log(odd);
+    }
+
+    function evenDisplay() {
+        if (odds === false) {
+        evens = true;
+        } else {
+            odds = false;
+            evens = true;
+        }
+        let i = 0;
+        even = [];
+        console.log(array.length);
+        while (i <= array.length) {
+            if (array[i] % 2 === 0) {
+                even.push(array[i]);
+            }
+            i++;
+        }
+        console.log(even);
+    }
+    return (
+        <>
+            <header>
+                <div className="heading">NUMBER FILTER</div>
+            </header>
+            <br></br>
+            <div>
+                <input type="text" placeholder="Enter the number of inputs" className="numbers" onChange={getData}></input>
+            </div><br></br>
+            <div>
+                <button className="random" onClick={handle}>Generate Random Numbers</button>
+            </div><br></br>
+            <div className="buttons">
+                <button onClick={oddDisplay}>ODD</button>&emsp;<span><button onClick={evenDisplay}>EVEN</button></span>
+            </div>
+            <div>
+                <h2 className="heading">Filtered Numbers</h2>
+                {odds ? <h1>{odd.join(", ")}</h1> : <h1>{even.join(", ")}</h1> }
+            </div>
+        </>
+    )
 }
+}
+
 
 export default App;
